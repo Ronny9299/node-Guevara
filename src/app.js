2c285cd541fee7dea5d913a77f8141ae56e185a4
Ronny9299 <ronnyguevara2012@hotmail.com>
2021-09-03 23:28:45 -0500

    Creacion de rutas y controladores de producto

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+import pkg from '../package-lock.json';
+
+import productRoutes from "./routes/products.routes";
+
+const app = express();
+
+app.set('pkg', pkg);
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.json({
+        author: app.get('pkg').name,
+        decription: app.get('pkg').decription,
+        version: app.get('pkg').version
+    })
+})
+
+app.use('/products', productRoutes);
+export default app;
---
+++ src/controllers/products.controller.js
@@ -0,0 +1,15 @@
+export const createProduct = (req, res) => {
+
+}
+export const getProducts = (req, res) => {
+    res.json('GET Products')
+}
+export const getProductById = (req, res) => {
+
+}
+export const updateProductById = (req, res) => {
+
+}
+export const deleteProductById = (req, res) => {
+
+}
