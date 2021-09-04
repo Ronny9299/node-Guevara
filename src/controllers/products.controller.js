b698d5261084a426e2ccf6515a757aa9b81253b5
Ronny9299 <ronnyguevara2012@hotmail.com>
2021-09-04 01:06:13 -0500

    CRUD de producto

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,16 +1,51 @@
 import Product from "../models/Product";
-export const createProduct = (req, res) => {
+export const createProduct = async(req, res) => {
+    const { name, category, price, imgURL } = req.body;
 
-}
-export const getProducts = (req, res) => {
-    res.json('GET Products')
-}
-export const getProductById = (req, res) => {
+    try {
+        const newProduct = new Product({
+            name,
+            category,
+            price,
+            imgURL,
+        });
 
-}
-export const updateProductById = (req, res) => {
+        const productSaved = await newProduct.save();
 
-}
-export const deleteProductById = (req, res) => {
+        res.status(201).json(productSaved);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+};
 
-}
+export const getProducts = async(req, res) => {
+    const products = await Product.find();
+    return res.json(products);
+};
+
+export const getProductById = async(req, res) => {
+    const { productId } = req.params;
+
+    const product = await Product.findById(productId);
+    res.status(200).json(product);
+};
+
+export const updateProductById = async(req, res) => {
+    const updatedProduct = await Product.findByIdAndUpdate(
+        req.params.productId,
+        req.body, {
+            new: true,
+        }
+    );
+    res.status(200).json(updatedProduct);
+};
+
+export const deleteProductById = async(req, res) => {
+    const { productId } = req.params;
+
+    await Product.findByIdAndDelete(productId);
+
+    // code 200 is ok too
+    res.status(200).json();
+};
