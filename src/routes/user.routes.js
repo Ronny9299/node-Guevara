444ccdaf7aba4be0355403309be68eca7e8cef6f
Ronny9299 <ronnyguevara2012@hotmail.com>
2021-09-05 00:59:02 -0500

    terminando  algunas rutas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,16 @@
-import { Router } from 'express';
-const router = Router();
+  import { Router } from "express";
+  const router = Router();
 
-export default router;
+  import * as usersCtrl from "../controllers/user.controller";
+  import { authJwt, verifySignup } from "../middlewares";
+
+  router.post(
+      "/", [
+          authJwt.verifyToken,
+          authJwt.isAdmin,
+          verifySignup.checkDuplicateUsernameOrEmail,
+      ],
+      usersCtrl.createUser
+  );
+
+  export default router;
