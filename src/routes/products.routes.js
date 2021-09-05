8bbd6b9b4f49c03f6050836a91dee092a4fe1232
Ronny9299 <ronnyguevara2012@hotmail.com>
2021-09-05 00:19:40 -0500

    verificacion del token en producto

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 const router = Router();
 import * as productsCtrl from "../controllers/products.controller";
-
-router.post('/', productsCtrl.createProduct);
+import { authJwt } from "../middlewares";
+router.post('/', authJwt.verifyToken, productsCtrl.createProduct);
 
 router.get('/', productsCtrl.getProducts);
 
 router.get('/:productId', productsCtrl.getProductById);
 
-router.put('/:productId', productsCtrl.updateProductById);
+router.put('/:productId', authJwt.verifyToken, productsCtrl.updateProductById);
 
-router.delete('/:productId', productsCtrl.deleteProductById);
+router.delete('/:productId', authJwt.verifyToken, productsCtrl.deleteProductById);
 
 export default router;
