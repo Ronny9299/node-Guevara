56da3c9484f066e7d0ed7543feabe4a4d67ee9bd
Ronny9299 <ronnyguevara2012@hotmail.com>
2021-09-04 23:19:44 -0500

    agregando algunas rutas

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from 'express';
 import morgan from 'morgan';
 import pkg from '../package-lock.json';
 
+import { createRoles } from "./libs/initialSetup";
+
 import productRoutes from "./routes/products.routes";
+import authRoutes from './routes/auth.routes';
 
 const app = express();
+createRoles();
 
 app.set('pkg', pkg);
 
@@ -16,8 +20,11 @@ app.get('/', (req, res) => {
         author: app.get('pkg').name,
         decription: app.get('pkg').decription,
         version: app.get('pkg').version
-    })
-})
+    });
+});
 
-app.use('/products', productRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+
+
 export default app;
